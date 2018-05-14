c6d231828e60a60fb741247dc4187eefdfb1d2f8
Shirleyson Kaisser <skaisser@gmail.com>
2018-05-13 22:11:04 -0300

    Footer Details

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,8 +2,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
+        <strong>Versão</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>Desenvolvido Por <a href="http://www.kapsula.com.br" target="_blank">Kapsula</a></strong>
 </footer>
